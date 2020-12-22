c9b97a44e689b29c3bf831a24b3bede7d50b6912
thisdk <thisdk@gmail.com>
2020-12-23 00:36:30 +0800

    引入组件,添加TabBar

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,36 @@
+Component({
+    data: {
+        active: 0,
+        list: [
+            {
+                icon: 'home-o',
+                text: '主页',
+                url: '/pages/index/index'
+            },
+            {
+                icon: 'orders-o',
+                text: '订单',
+                url: '/pages/order/order'
+            },
+            {
+                icon: 'user-o',
+                text: '我',
+                url: '/pages/user/user'
+            }
+        ]
+    },
+    methods: {
+        onChange(event) {
+            this.setData({active: event.detail});
+            wx.switchTab({
+                url: this.data.list[event.detail].url
+            });
+        },
+        init() {
+            const page = getCurrentPages().pop();
+            this.setData({
+                active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+            });
+        }
+    }
+})
